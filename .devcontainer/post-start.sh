3ffa0b86101c6163b37bfb8605ad0d0eedc9d9cc
Dan Bitter <HireDanBitter@gmail.com>
2026-07-30 15:27:35 -0400

    Fix typos

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -6,11 +6,11 @@ sudo /usr/local/bin/init-firewall.sh
 
 # # Claude Code API key setup
 # echo '{"apiKeyHelper": "/home/node/.claude/anthropic_key.sh"}' > /home/node/.claude/settings.json
-# printf '#!.bin.sh\necho $ANTHROPIC_API_KEY' > /home/node/.claude/anthropic_key.sh
+# printf '#!/bin/sh\necho $ANTHROPIC_API_KEY' > /home/node/.claude/anthropic_key.sh
 # chmod +x /home/node/.claude/anthropic_key.sh
 
 # Git
-git config --glob al --add safe.directory /workspace
+git config --global --add safe.directory /workspace
 
 # Run project setup
 bash /workspace/.devcontainer/script-00-setup.sh
